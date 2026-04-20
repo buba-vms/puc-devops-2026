36b1f5b4970d394366e67a1f409b5608f6bb8b77
Victor Buba <victor.buba@live.com>
2026-04-20 01:50:04 -0300

    separando app do servidor para facilitar os testes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const healthRouter = require('./routes/health');
-const productsRouter = require('./routes/products');
+const app = require('./app');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.use('/health', healthRouter);
-app.use('/produtos', productsRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
